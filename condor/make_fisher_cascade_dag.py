73deeeff78472cd1d474784a79205b4bf2a34fa0
Christian Haack <chr.hck@gmail.com>
2022-02-08 15:26:54 +0000

    dag fix

--- condor/make_fisher_cascade_dag.py
+++ condor/make_fisher_cascade_dag.py
@@ -13,23 +13,21 @@ counts_model_path = (
     "/data/p-one/chaack/hyperion/data/photon_arrival_time_counts_params.pickle"
 )
 
-outfile_path = "/data2/p-one/chaack/fisher/fisher_$(spacing)_$(energy)_$(seed).npz"
+outfile_path = "fisher_$(spacing)_$(energy)_$(seed).npz"
 
 description = htcondor.Submit(
-    executable="/bin/env",  # the program we want to run
+    executable="/data/p-one/chaack/run.sh",  # the program we want to run
     arguments=f"python /data/p-one/chaack/olympus/run_fisher.py -o {outfile_path} -s $(spacing) -e $(energy) --seed $(seed) --shape_model {shape_model_path} --counts_model {counts_model_path}",
     log="logs/log",  # the HTCondor job event log
-    output="logs/fisher.out.$(spacing)_$(energy)",  # stdout from the job goes here
-    error="ogs/fisher.err.$(spacing)_$(energy)",  # stderr from the job goes here
+    output="logs/fisher.out.$(spacing)_$(energy)_$(seed)",  # stdout from the job goes here
+    error="logs/fisher.err.$(spacing)_$(energy)_$(seed)",  # stderr from the job goes here
     request_gpus="1",  # resource requests; we don't need much per job for this problem
-    **{
-        "+SingularityImage": classad.quote(
-            "/data/p-one/chaack/container/pytorch-geo.sif"
-        ),
-    }
-    # request_memory="128MB",
-    # request_disk="1GB",
-)
+    Requirements="HasSingularity",
+    should_transfer_files="YES",
+    when_to_transfer_output="ON_EXIT"
+
+    )
+description["+SingularityImage"] = classad.quote("/data/p-one/pytorch-geo.sif")
 
 spacings = np.linspace(50, 200, 10)
 energies = np.logspace(3, 5.5, 10)
