415df85cc239cdec176ca165e9ca9035ffa148f1
Christian Haack <christian.haack@tum.de>
2022-02-08 16:30:30 +0100

    updated dag

--- condor/make_fisher_cascade_dag.py
+++ condor/make_fisher_cascade_dag.py
@@ -17,25 +17,25 @@ outfile_path = "fisher_$(spacing)_$(energy)_$(seed).npz"
 
 description = htcondor.Submit(
     executable="/data/p-one/chaack/run.sh",  # the program we want to run
-    arguments=f"python /data/p-one/chaack/olympus/run_fisher.py -o {outfile_path} -s $(spacing) -e $(energy) --seed $(seed) --shape_model {shape_model_path} --counts_model {counts_model_path}",
+    arguments=f"python /data/p-one/chaack/olympus/run_fisher.py -o {outfile_path} -s $(spacing) -e $(energy) --seed $(seed) --shape_model {shape_model_path} --counts_model {counts_model_path} --pmts $(pmts)",
     log="logs/log",  # the HTCondor job event log
     output="logs/fisher.out.$(spacing)_$(energy)_$(seed)",  # stdout from the job goes here
     error="logs/fisher.err.$(spacing)_$(energy)_$(seed)",  # stderr from the job goes here
     request_gpus="1",  # resource requests; we don't need much per job for this problem
     Requirements="HasSingularity",
     should_transfer_files="YES",
-    when_to_transfer_output="ON_EXIT"
-
-    )
+    when_to_transfer_output="ON_EXIT",
+)
 description["+SingularityImage"] = classad.quote("/data/p-one/pytorch-geo.sif")
 
-spacings = np.linspace(50, 200, 10)
-energies = np.logspace(3, 5.5, 10)
+spacings = np.linspace(50, 200, 7)
+energies = np.logspace(3, 5.5, 7)
 seeds = np.arange(100)
+pmts = [16, 20, 24]
 
 dagvars = []
-for spacing, energy, seed in product(spacings, energies, seeds):
-    dagvars.append({"spacing": spacing, "energy": energy, "seed": seed})
+for spacing, energy, seed, pmt in product(spacings, energies, seeds, pmts):
+    dagvars.append({"spacing": spacing, "energy": energy, "seed": seed, "pmts": pmt})
 
 layer = dag.layer(
     name="fisher",
