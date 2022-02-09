b329dc61bd9af66c100972de3708a3cdda4e0e63
Christian Haack <christian.haack@tum.de>
2022-02-09 08:51:21 +0000

    update combiner to deal with dicts

--- combine_fisher.py
+++ combine_fisher.py
@@ -1,5 +1,5 @@
 from argparse import ArgumentParser
-import numpy as np
+import pickle
 
 parser = ArgumentParser()
 parser.add_argument("-i", nargs="+", dest="infiles", required=True)
@@ -7,17 +7,9 @@ parser.add_argument("-o", dest="outfile", required=True)
 
 args = parser.parse_args()
 
-spacings = []
-energies = []
-fishers = []
+
+data = []
 for f in args.infiles:
-    arr = np.load(f)
-    spacings.append(arr["spacing"])
-    energies.append(arr["energy"])
-    fishers.append(arr["fisher"])
+    data.append(pickle.load(open(f, "rb")))
 
-spacings = np.asarray(spacings)
-energies = np.asarray(energies)
-fishers = np.asarray(fishers)
-
-np.savez(args.outfile, spacing=spacings, energy=energies, fisher=fishers)
+pickle.dump(data, open(args.outfile, "wb"))
