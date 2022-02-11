4e49dfbc2dc035faaa30bcb82ebd3b76c021caee
Christian Haack <christian.haack@tum.de>
2022-02-11 10:10:50 +0000

    updated combiner

--- combine_fisher.py
+++ combine_fisher.py
@@ -13,9 +13,9 @@ data = []
 for f in args.infiles:
 
     # Hotfix
-    pmts = re.match(".*_([0-9]*)_", f).groups()[0]
+    # pmts = re.match(".*_([0-9]*)_", f).groups()[0]
     d = pickle.load(open(f, "rb"))
-    d["pmts"] = int(pmts)
+    # d["pmts"] = int(pmts)
     data.append(d)
 
 pickle.dump(data, open(args.outfile, "wb"))
