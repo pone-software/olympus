2315bd6f00c75facf3562731999190878ee9489b
Christian Haack <christian.haack@tum.de>
2022-02-21 11:01:19 +0100

    bugfix

--- olympus/event_generation/photon_propagation/utils.py
+++ olympus/event_generation/photon_propagation/utils.py
@@ -72,7 +72,7 @@ def bucket_fn(bucket_size, use_bayesian_blocks=True):
                 if bucket_ix == 0:
                     log2_pad_len = self._buckets[1]
                 elif bucket_ix == len(self._buckets):
-                    log2_pad_len = np.ceil(np.log(data_size) / np.log(bucket_size))
+                    log2_pad_len = np.ceil(lds)
                 else:
                     log2_pad_len = self._buckets[bucket_ix]
                 pad_len = int(np.ceil(np.power(2, log2_pad_len)))
